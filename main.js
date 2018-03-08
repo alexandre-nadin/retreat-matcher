8dc3faa560e8bdd9823829b7742aecd64bf7b5d5
Alexandre Nadin <alex.nadin12@gmail.com>
2018-03-08 16:23:27 +0100

    Init project

---
+++ main.js
@@ -0,0 +1,9 @@
+function fetchIssues() {
+  console.log("[fetchIssues] ");
+  let txt = document.getElementById("text-name-selection")
+  console.log("  txt: ", txt);
+}
+
+function initBody() {
+  console.log("[initBody]");
+}
